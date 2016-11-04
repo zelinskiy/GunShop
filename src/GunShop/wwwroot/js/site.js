749bf5e67d18ae94bfebef71e96ca9ad4d0f780a
zelinskiy <zelinskiy1917@gmail.com>
2016-11-04 11:10:24 +0200

    extendable inputs list

--- src/GunShop/wwwroot/js/site.js
+++ src/GunShop/wwwroot/js/site.js
@@ -1,8 +1,24 @@
 ﻿
-angular.module('my-app', [], function () {
+angular.module('my-app', [], function () { })
+    .controller('TestController', TestController)
 
-}).controller('TestController', TestController)
 
 function TestController($scope) {
-    $scope.list = [{ value: 'value' }, { value: 'value' }, { value: 'value' }];
+    $scope.charsList = [];
+
+    $scope.extendDown = function (e) {
+        if (e.keyCode == 13) {
+            console.log(e.delegateTarget.value);
+            $scope.charsList.push({ name: e.delegateTarget.value });
+            e.delegateTarget.value = "";
+        }        
+    }
+
+    $scope.chk = function () { console.log($scope.charsList); }
 }
+
+var textInputtedEvent = new CustomEvent("textInputtedEvent", {});
+
+$(document).ready(function () {
+    
+})
